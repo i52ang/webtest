8bb0ea9522498d94eef95ec04438c256e8d17638
i52ang <qqq139574361726@163.com>
2021-05-25 10:06:29 +0800

    add react route and react bootstrap

--- src/main/webapp/react/app.js
+++ src/main/webapp/react/app.js
@@ -2,7 +2,9 @@ seajs.config({
 	base: './react',
 	alias: {
 		react: '../bower_components/react/react.js',
-		showdown: '../bower_components/showdown/src/showdown.js'
+		showdown: '../bower_components/showdown/src/showdown.js',
+		router: '../bower_components/react-router/dist/react-router.js',
+		bootstrap: '../bower_components/react-bootstrap/react-bootstrap.js'
 	}
 });
 
